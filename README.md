e12db4859a0ba61900241e0284fa208afc2e782f
ejkim000 <developer.ejk@gmail.com>
2023-06-26 12:09:48 -0400

    add game.html & enter username part of  js

--- README.md
+++ README.md
@@ -6,3 +6,19 @@
 3. Wong answer doesn't count
 4. Click "Skip" button when you dont know the answer
 
+## Classes
+1. Users
+
+2. Questions
+
+## Local Storage
+1. Users
+    - name: string, key value
+    - questions: array, [question id, correct(boolean)]
+    - clear_time: numbers, secondsof clear the game
+    - rank: number
+2. Questions
+    - question id: number
+    - question : string
+    - answer : string
+    
--- app.js
+++ app.js
@@ -1,21 +1,66 @@
-const start_btn = document.getElementById("start_btn");
-const users = JSON.parse(localStorage.getItem('users'));
-const names = [];
+class User {
+    constructor(user_name) {
+        this.user_name = user_name;
 
-// when click Start button
-start_btn.addEventListener('click', e =>{
-    e.preventDefault();
 
-    // check user input the name
-    // get names in the local storage
-    if (users.length > 0) {
-        users.forEach(user => names.push(user.name));
     }
-    // check the name is already taken
 
-    
-    // save name in the local storage
-    
-    // redirect to the game page
+    getUsers = () => {
+        return JSON.parse(localStorage.getItem('users'));
+    }
 
-});
+    static startGame = () => {
+
+        const users = JSON.parse(localStorage.getItem('users'));
+        let names = {};
+        let user = {};
+
+        // check user input the name
+        const user_name = document.getElementById('user_name').value;
+
+        if (!user_name) {
+            alert('Please enter your name');
+            return;
+        }
+        // get names in the local storage
+        if (users && users.length > 0) {
+            users.forEach(user => {
+                names.push(user.name);
+            });
+        }
+        // check the name is already taken 
+        if (names.length > 0 && names.includes(user_name)) {
+            alert('You name is already taken. Please enter other name');
+            user_name = '';
+            document.getElementById('name').focus;
+            return;
+        } else {
+            // save user in the local storage
+            user = {
+                'name': user_name,
+                'questions': [],
+                'clear_time': 0,
+                'rank': 0
+            };
+
+            localStorage.setItem('users', JSON.stringify(user));
+        }
+
+        // redirect to the game page
+        location.href = 'game.html';
+    }
+}
+
+
+
+if (document.getElementById("start_btn")) {
+    const start_btn = document.getElementById("start_btn");
+
+    // when click Start button
+    start_btn.addEventListener('click', e => {
+        e.preventDefault();
+
+        User.startGame();
+    });
+}
+
