32727f88bccf6d41292f2527e68d8a2727ef0935
ejkim000 <developer.ejk@gmail.com>
2023-06-26 12:58:22 -0400

    create User class and check user's name before start the game

--- app.js
+++ app.js
@@ -1,58 +1,41 @@
 class User {
     constructor(user_name) {
         this.user_name = user_name;
-
-
+        this.users = (localStorage.getItem('users')) ? JSON.parse(localStorage.getItem('users')) : [];
+        this.user = {
+            'name': this.user_name,
+            'questions': [],
+            'clear_time': 0,
+            'rank': 0
+        };
     }
 
-    getUsers = () => {
-        return JSON.parse(localStorage.getItem('users'));
-    }
-
-    static startGame = () => {
-
-        const users = JSON.parse(localStorage.getItem('users'));
-        let names = {};
-        let user = {};
-
-        // check user input the name
-        const user_name = document.getElementById('user_name').value;
-
-        if (!user_name) {
-            alert('Please enter your name');
-            return;
-        }
-        // get names in the local storage
-        if (users && users.length > 0) {
-            users.forEach(user => {
-                names.push(user.name);
+    // Check user's name before game start
+    checkUser = () => {
+        // get users' name
+        let names = [];
+        if (this.users) {
+            this.users.forEach(u => {
+                names.push(u.name);
             });
         }
-        // check the name is already taken 
-        if (names.length > 0 && names.includes(user_name)) {
-            alert('You name is already taken. Please enter other name');
-            user_name = '';
-            document.getElementById('name').focus;
+
+        // check the user name is already taken or not
+        if (names.length > 0 && names.includes(this.user_name)) {
+            document.getElementById('msg').innerText = `"${this.user_name}" is already taken. Please enter other name.`;
             return;
         } else {
-            // save user in the local storage
-            user = {
-                'name': user_name,
-                'questions': [],
-                'clear_time': 0,
-                'rank': 0
-            };
-
-            localStorage.setItem('users', JSON.stringify(user));
+            // add this user to local storage
+            this.users.push(this.user);
+            localStorage.setItem('users', JSON.stringify(this.users));
         }
-
         // redirect to the game page
         location.href = 'game.html';
     }
 }
 
 
-
+// Check user's name before start the game
 if (document.getElementById("start_btn")) {
     const start_btn = document.getElementById("start_btn");
 
@@ -60,7 +43,17 @@ if (document.getElementById("start_btn")) {
     start_btn.addEventListener('click', e => {
         e.preventDefault();
 
-        User.startGame();
+        // check user input the name or not
+        const user_name = document.getElementById('user_name').value;
+
+        if (!user_name) {
+            document.getElementById('msg').innerText = 'Please enter your name.';
+            return;
+        }
+
+        // check existing user
+        let user = new User(user_name);
+        user.checkUser();
     });
 }
 
